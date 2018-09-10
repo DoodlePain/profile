5ec5dbd2080b59fcea498c4bbd1a5f3524eca715
DoodlePain <manuelfrulloscarapazzi@gmail.com>
2018-09-11 00:36:25 +0200

    backside of the photo

--- src/Polaroid.js
+++ src/Polaroid.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import "./Polaroid.css";
 import ProfilePhoto from "./Profile.png";
+import { SocialIcon } from "react-social-icons";
 
 class Polaroid extends Component {
   state = {
-    flashActive: false
+    flashActive: false,
+    rotate: false
   };
   render() {
     return (
@@ -18,7 +20,10 @@ class Polaroid extends Component {
           <button
             className="fireButton"
             onClick={() => {
-              this.setState({ flashActive: !this.state.flashActive });
+              this.setState({
+                flashActive: !this.state.flashActive,
+                rotate: false
+              });
             }}
           >
             {" "}
@@ -43,7 +48,13 @@ class Polaroid extends Component {
           </div>
         </div>
         <div className="Shadow" />
-        <div className={this.state.flashActive ? "profileRight" : "profile"}>
+        <div
+          className={this.state.flashActive ? "profileRight" : "profile"}
+          id={this.state.rotate ? "rotateCard" : "normalCard"}
+          onClick={() => {
+            this.setState({ rotate: !this.state.rotate });
+          }}
+        >
           <img
             className={this.state.flashActive ? "image" : "showImage"}
             src={ProfilePhoto}
@@ -52,7 +63,57 @@ class Polaroid extends Component {
           <div className={this.state.flashActive ? "photo" : "photoShow"} />
           <h1 className="first">Hello, I'm Manuel :)</h1>
           <h1 className="second">I'm a Full-Stack developer</h1>
-          <h1 className="third">Click me :)</h1>
+          <h1 className="third">Tap :)</h1>
+        </div>
+        <div
+          className={this.state.rotate ? "backCard" : "hidden"}
+          onClick={() => {
+            this.setState({ rotate: !this.state.rotate });
+          }}
+        >
+          <h1 className="firstBack">
+            I'm a young Web Developer from Rome, Italy.
+            <br />
+            I'm studying Computer Science at Tor Vergata University in Rome.
+            <br />
+            <br />I mainly work with javascript with frameworks, such as
+            React.js
+            <br />
+            but I'm very flexible and not afraid to try anything else.
+          </h1>
+          <h1 className="secondBack">
+            Contact me :)
+            <br />
+            Manuel Scarapazzi
+          </h1>
+        </div>
+        <div className="Footer">
+          {"Made with ♥ in Rome"}
+          <SocialIcon
+            color="#fff"
+            style={{ height: 25, width: 25, "margin-left": "5px" }}
+            url="http://twitter.com/DoodlePain"
+          />
+          <SocialIcon
+            color="#fff"
+            style={{ height: 25, width: 25, "margin-left": "5px" }}
+            url="https://www.linkedin.com/in/manuel-scarapazzi-514868118/"
+          />
+          <SocialIcon
+            color="#fff"
+            style={{ height: 25, width: 25, "margin-left": "5px" }}
+            url="https://www.facebook.com/ZioBanana"
+          />
+          <SocialIcon
+            style={{ height: 25, width: 25, "margin-left": "5px" }}
+            color="#fff"
+            url="https://www.instagram.com/frullo_e_peroni/?hl=it"
+          />
+          <SocialIcon
+            style={{ height: 25, width: 25, "margin-left": "5px" }}
+            color="#fff"
+            url="https://github.com/DoodlePain"
+          />
         </div>
       </div>
     );
